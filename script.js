6d7175b4c7c84752f610ce196bbcd982fbb42842
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-03-05 00:34:53 +0300

    declared calculations on js file

---
+++ script.js
@@ -0,0 +1,41 @@
+var maleNames = [ 'Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi',  'Kwame' ];
+var femaleNames = ['Akosua',  'Adwoa', 'Abenaa', 'Akua',  'Yaa', 'Afua', 'Ama'];
+
+function check(){
+    var gender = document.getElementById('gender').value;
+    var day = document.getElementById('day').value;
+    var dd = perseInt(day);
+    var month = document.getElementById('month').value;
+    var mm = perseInt(month);
+    var year = document.getElementById('year').value;
+    var yy = perseInt(year.substr(2.4));
+    var yy = perseInt(year.substr(0.2));
+
+   var dayWeek = Maths.abs(parseInt(((cc/4) -2*cc-1) + ((5*yy/4)) + ))
+
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  
+   
+    
+    
+    
+   
+    
+     
+
+    
+   
