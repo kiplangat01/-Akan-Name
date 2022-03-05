e5e483438de3982ab45c576449c2d78b7c46891d
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-03-05 21:51:09 +0300

    ft:made the app interactive

--- script.js
+++ script.js
@@ -2,17 +2,32 @@ var maleNames = [ 'Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi',  'Kwame'
 var femaleNames = ['Akosua',  'Adwoa', 'Abenaa', 'Akua',  'Yaa', 'Afua', 'Ama'];
 
 function check(){
-    var gender = document.getElementById('gender').value;
-    var day = document.getElementById('day').value;
-    var dd = perseInt(day);
-    var month = document.getElementById('month').value;
-    var mm = perseInt(month);
-    var year = document.getElementById('year').value;
-    var yy = perseInt(year.substr(2.4));
-    var yy = perseInt(year.substr(0.2));
+   var gender = document.getElementById("gender").value;
+   var day = document.getElementById("day").value;
+   var dd = parseInt(day);
+   var month = document.getElementById("month").value;
+   var mm = parseInt(month);
+   var year = document.getElementById("year").value;
+   var yy = parseInt(year.substr(2, 4));
+   var cc = parseInt(year.substr(0, 2));
 
-   var dayWeek = Maths.abs(parseInt(((cc/4) -2*cc-1) + ((5*yy/4)) +  ))
+   var birthDay = parseInt(((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd)%7;
 
+   if (dd <= 0 || dd > 31){
+       alert("enter valid date");
+   } 
+   else if (mm <= 0 || mm > 12){
+    alert("enter valid month");
+} 
+   else if (year <= 0){
+    alert("enter valid year");
+} 
+if (gender === "male"){
+    alert("your name is " + maleNames[birthDay]);
+}
+  else if (gender === "female"){
+    alert("your name is " + femaleNames[birthDay]);
+}
 }
 
     
